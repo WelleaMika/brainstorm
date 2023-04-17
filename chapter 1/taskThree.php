d8fc88e6d3bdfbbed0c59995b0e005612f06eefe
WelleaMika <125141235+WelleaMika@users.noreply.github.com>
2023-04-17 16:02:30 +0300

    Update taskThree.php

--- chapter 1/taskThree.php
+++ chapter 1/taskThree.php
@@ -7,30 +7,26 @@
 
 $arr = array();
 
-for ($i = 1000; $i <= 9999; $i++)    
-{
-    if($i % 2 == 1)
-    {
+for ($i = 1000; $i <= 9999; $i++) {
+    if($i % 2 == 1) {
         continue;
     }
+    
     $number = $i;
     $increase = 0;
     $decrease = 0;
-    while ($number > 9)
-    {
+    
+    while ($number > 9) {
         $firstDigit = $number % 10;
         $number = (int) ($number / 10);
-        if(($number % 10) <= ($firstDigit))
-        {
+        if(($number % 10) <= ($firstDigit)) {
             $increase++;
         }
-        if(($number % 10) >= ($firstDigit))
-        {
+        if(($number % 10) >= ($firstDigit)) {
             $decrease++;
         }
     }
-    if(($increase == 0) || ($decrease == 0))
-    {
+    if(($increase == 0) || ($decrease == 0)) {
         $arr[] = $i;
     }
 }
